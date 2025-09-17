1d4c32c933638aeb5a95eeb328417a2af77cbd6f
Gabriel Santos santana <santossantanag0@gmail.com>
2025-09-17 13:04:01 +0000

    amenta mais esse delay

--- src/components/welcome-screen.tsx
+++ src/components/welcome-screen.tsx
@@ -13,7 +13,7 @@ const WelcomeScreen = ({ isVisible, onShowMemories }: WelcomeScreenProps) => {
   const handleClick = () => {
     setTimeout(() => {
       onShowMemories();
-    }, 800);
+    }, 1200);
   };
 
   return (
