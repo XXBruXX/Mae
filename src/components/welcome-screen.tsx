c1a740268e5af0fabf142246f821f8c1a52e2bfa
Gabriel Santos santana <santossantanag0@gmail.com>
2025-09-17 12:28:50 +0000

    sim e nao mudou a font

--- src/components/welcome-screen.tsx
+++ src/components/welcome-screen.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { Button } from './ui/button';
 import { SparklesPreview } from './sparkles';
 import { SparkleButton } from './ui/sparkle-button';
 
